744d0e6d74c08436e92393b176d377b0b2103db3
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2026-01-01 03:58:21 +0700

    Simplify test

--- test/source/test.cpp
+++ test/source/test.cpp
@@ -5,8 +5,6 @@
 
 #include <boost/ut.hpp>
 
-#include <source_location>
-
 namespace ut = boost::ut;
 
 template <typename Fn>
@@ -49,31 +47,29 @@ struct linr::CustomParser<Idk>
     }
 };
 
-void test(auto&& read, std::source_location loc = std::source_location::current())
+// default reader, no buf
+struct DefReader
 {
-    using namespace ut::literals;
+    auto read(auto&&... args)
+    {
+        return linr::read(std::forward<decltype(args)>(args)...);    //
+    }
 
-    const auto name = std::format("[{}:{}]", loc.file_name(), loc.line());
+    template <typename... Ts>
+        requires (sizeof...(Ts) >= 1)
+    auto read(auto&&... args)
+    {
+        return linr::read<Ts...>(std::forward<decltype(args)>(args)...);
+    }
+};
 
-    ut::test("fundamental types can be parsed" + name) = [] {
-        static_assert(linr::Parseable<bool>);
-        static_assert(linr::Parseable<char>);
-        static_assert(linr::Parseable<unsigned char>);
-        static_assert(linr::Parseable<signed char>);
-        static_assert(linr::Parseable<int>);
-        static_assert(linr::Parseable<unsigned int>);
-        static_assert(linr::Parseable<long>);
-        static_assert(linr::Parseable<unsigned long>);
-        static_assert(linr::Parseable<long long>);
-        static_assert(linr::Parseable<unsigned long long>);
-    };
-
-    ut::test("custom type can be made Parseable" + name) = [] {
-        static_assert(linr::Parseable<Idk>);    //
-    };
+template <typename R>
+void test(R&& reader)
+{
+    const auto name = std::format(" [{}]", ut::reflection::type_name<R>());
 
     ut::test("read a string, a whole line of it" + name) = [&] {
-        auto value = read.template operator()<>("insert a string: ").value();
+        auto value = reader.read("insert a string: ").value();
         std::cout << "value: " << value << '\n';
 
         // > read call above is equivalent to
@@ -81,22 +77,22 @@ void test(auto&& read, std::source_location loc = std::source_location::current(
     };
 
     ut::test("read a string until delimiter reached" + name) = [&] {
-        auto value = read.template operator()<std::string>("a string: ").value();
+        auto value = reader.template read<std::string>("a string: ").value();
         std::cout << "value: " << value << '\n';
     };
 
     ut::test("read a single char value" + name) = [&] {
-        auto value = read.template operator()<char>("a char: ").value();
+        auto value = reader.template read<char>("a char: ").value();
         std::cout << "value: " << value << '\n';
     };
 
     ut::test("multiple valued read - same type" + name) = [&] {
-        auto [i1, i2, i3] = read.template operator()<int, int, int>("3 int: ").value();
+        auto [i1, i2, i3] = reader.template read<int, int, int>("3 int: ").value();
         std::cout << "value: " << i1 << ", " << i2 << ", " << i3 << '\n';
     };
 
     ut::test("multiple valued read - different types" + name) = [&] {
-        auto [i1, d, i2] = read.template operator()<int, double, int>("an int, double and int: ").value();
+        auto [i1, d, i2] = reader.template read<int, double, int>("an int, double and int: ").value();
         std::cout << "value: " << i1 << ", " << d << ", " << i2 << '\n';
     };
 
@@ -104,7 +100,7 @@ void test(auto&& read, std::source_location loc = std::source_location::current(
     ut::test("read value until condition met" + name) = [&] {
         int result = 0;
         while (true) {
-            auto value = read.template operator()<int>("please enter an integer: ");
+            auto value = reader.template read<int>("please enter an integer: ");
             if (value) {
                 result = value.value();
                 break;
@@ -124,12 +120,12 @@ void test(auto&& read, std::source_location loc = std::source_location::current(
     };
 
     ut::test("read custom struct" + name) = [&] {
-        auto value = read.template operator()<Idk>("enter int and float separated by spaces: ", '\n').value();
+        auto value = reader.template read<Idk>("enter int and float separated by spaces: ", '\n').value();
         std::cout << "value: " << value.m_int << " | " << value.m_float << '\n';
     };
 
     ut::test("read getline then parse the line into custom struct" + name) = [&] {
-        auto str = read.template operator()<>("enter int and float separated by spaces: ").value();
+        auto str = reader.read("enter int and float separated by spaces: ").value();
 
         std::cout << ">>>>> " << str << '\n';
 
@@ -140,19 +136,23 @@ void test(auto&& read, std::source_location loc = std::source_location::current(
 
 int main()
 {
-    test([]<typename... T>(std::string_view prompt, char delim = ' ') {
-        if constexpr (sizeof...(T) == 0) {
-            return linr::read(prompt);
-        } else {
-            return linr::read<T...>(prompt, delim);
-        }
-    });
+    ut::test("fundamental types can be parsed") = [] {
+        static_assert(linr::Parseable<bool>);
+        static_assert(linr::Parseable<char>);
+        static_assert(linr::Parseable<unsigned char>);
+        static_assert(linr::Parseable<signed char>);
+        static_assert(linr::Parseable<int>);
+        static_assert(linr::Parseable<unsigned int>);
+        static_assert(linr::Parseable<long>);
+        static_assert(linr::Parseable<unsigned long>);
+        static_assert(linr::Parseable<long long>);
+        static_assert(linr::Parseable<unsigned long long>);
+    };
 
-    test([reader = linr::BufReader(1024)]<typename... T>(std::string_view prompt, char delim = ' ') mutable {
-        if constexpr (sizeof...(T) == 0) {
-            return reader.read(prompt);
-        } else {
-            return reader.read<T...>(prompt, delim);
-        }
-    });
+    ut::test("custom type can be made Parseable") = [] {
+        static_assert(linr::Parseable<Idk>);    //
+    };
+
+    test(DefReader{});
+    test(linr::BufReader{ 1024 });
 }
